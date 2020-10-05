8649580df09ca9d7258afcb64bce2ca85b71b98b
Dantara <supercreed2@gmail.com>
2020-10-05 20:50:54 +0300

    Servant NameServer

--- shared/API/NameServer.hs
+++ shared/API/NameServer.hs
@@ -15,9 +15,18 @@ type NameServerAPI = "init" :> Get '[JSON] SystemStatus
                 :<|> "file" :> FileAPI
                 :<|> "dir" :> DirAPI
 
-type FileAPI = "create" :> ReqBody '[JSON] ByteString :> Post '[JSON] FileStatus
+type FileAPI = "create" :> ReqBody '[JSON] FilePath :> Post '[JSON] FileStatus
+          :<|> "read" :> ReqBody '[JSON] FilePath :> Post '[JSON] IP
+          :<|> "write" :> ReqBody '[JSON] FilePath :> Post '[JSON] IPList
+          :<|> "delete" :> ReqBody '[JSON] FilePath :> Post '[JSON] FileStatus
+          :<|> "info" :> QueryParam "file" FilePath :> Get '[JSON] FileInfo
+          :<|> "copy" :> ReqBody '[JSON] FilePath :> Post '[JSON] FileStatus
+          :<|> "move" :> ReqBody '[JSON] FilePath :> Post '[JSON] FileStatus
 
-type DirAPI = "create" :> ReqBody '[JSON] ByteString :> Post '[JSON] DirStatus
+type DirAPI = "create" :> ReqBody '[JSON] DirPath :> Post '[JSON] DirStatus
+         :<|> "delete" :> ReqBody '[JSON] DirPath :> Post '[JSON] DirStatus
+         :<|> "info" :> QueryParam "dir" DirPath :> Get '[JSON] DirInfo
+         :<|> "exists" :> QueryParam "dir" DirPath :> Get '[JSON] DirStatus
 
 data FileStatus = FileSuccess | FileError FileError
 
@@ -35,7 +44,18 @@ data DirError
   | IncorrectDirPath
   | CustomDirError ByteString
 
-data SystemStatus = SystemStatus SystemSize
+newtype SystemStatus = SystemStatus Size
 
-newtype SystemSize = SystemSize Integer
+newtype Size = Size Integer
 
+newtype IP = IP ByteString
+newtype IPList = IPList [IP]
+
+newtype ServerName = ServerName ByteString
+
+data FileInfo = FileInfo Size [ServerName]
+
+newtype DirPath = DirPath String
+newtype FileName = FileName ByteString
+
+newtype DirInfo = DirInfo [FileName]
