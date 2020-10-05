07a6822157d43c38713dc23366f4d8db5d164c4d
Mike <super.mvk@yandex.ru>
2020-10-06 01:13:58 +0300

    Add StorageServer API

--- shared/API/StorageServer.hs
+++ shared/API/StorageServer.hs
@@ -1,5 +1,34 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+
 module API.StorageServer where
 
-import           Servant.API
+import API.NameServer (DirPath, DirStatus, FileStatus, SystemStatus)
+import Servant.API
 
 -- Here we will define api for Storage Server
+
+-- Here we will define api for Name Server
+
+type StorageServerAPI =
+  "init" :> Get '[JSON] SystemStatus
+    :<|> "tree" :> Get '[JSON] StorageState
+    :<|> "status" :> Get '[NoContent] ()
+    :<|> "file" :> FileAPI
+    :<|> "dir" :> DirAPI
+
+type FileAPI =
+  "create" :> ReqBody '[JSON] FilePath :> Post '[JSON] FileStatus
+    :<|> "read" :> ReqBody '[JSON] FilePath :> Post '[JSON] Raw
+    :<|> "write" :> ReqBody '[JSON] FilePath :> Post '[JSON] Raw
+    :<|> "delete" :> ReqBody '[JSON] FilePath :> Post '[JSON] FileStatus
+    :<|> "copy" :> ReqBody '[JSON] FilePath :> Post '[JSON] FileStatus
+    :<|> "move" :> ReqBody '[JSON] FilePath :> Post '[JSON] FileStatus
+
+type DirAPI =
+  "create" :> ReqBody '[JSON] DirPath :> Post '[JSON] DirStatus
+    :<|> "delete" :> ReqBody '[JSON] DirPath :> Post '[JSON] DirStatus
+
+newtype StorageState = StorageState [String]
