fb2a3c5a5a201e229f09eacf5d76d92d36d4f72c
Dantara <supercreed2@gmail.com>
2020-10-05 20:15:08 +0300

    Servant started

--- shared/API/NameServer.hs
+++ shared/API/NameServer.hs
@@ -1,3 +1,41 @@
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators  #-}
+
 module API.NameServer where
 
+import           Data.Aeson
+import           Data.ByteString
+import           Servant.API
+
 -- Here we will define api for Name Server
+
+type NameServerAPI = "init" :> Get '[JSON] SystemStatus
+                :<|> "file" :> FileAPI
+                :<|> "dir" :> DirAPI
+
+type FileAPI = "create" :> ReqBody '[JSON] ByteString :> Post '[JSON] FileStatus
+
+type DirAPI = "create" :> ReqBody '[JSON] ByteString :> Post '[JSON] DirStatus
+
+data FileStatus = FileSuccess | FileError FileError
+
+data FileError
+  = FileExists
+  | FileDoesNotExist
+  | IncorrectFilePath
+  | CustomFileError ByteString
+
+data DirStatus = DirSuccess | DirError DirError
+
+data DirError
+  = DirExists
+  | DirDoesNotExist
+  | IncorrectDirPath
+  | CustomDirError ByteString
+
+data SystemStatus = SystemStatus SystemSize
+
+newtype SystemSize = SystemSize Integer
+
--- shared/API/StorageServer.hs
+++ shared/API/StorageServer.hs
@@ -1,3 +1,5 @@
 module API.StorageServer where
 
+import           Servant.API
+
 -- Here we will define api for Storage Server
