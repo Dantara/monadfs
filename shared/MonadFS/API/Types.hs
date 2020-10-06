07f3e44ba377fe14ffd70792acda1f84af13cb68
Dantara <supercreed2@gmail.com>
2020-10-06 15:38:59 +0300

    aeson instances deriving

--- shared/MonadFS/API/Types.hs
+++ shared/MonadFS/API/Types.hs
@@ -1,37 +1,67 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
+
 module MonadFS.API.Types where
 
+import           Data.Aeson
 import           Data.ByteString
+import           Data.Text
+import           GHC.Generics
 
 data FileStatus
   = FileSuccess
   | FileError FileError
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
+
 
 data FileError
   = FileExists
   | FileDoesNotExist
   | IncorrectFilePath
-  | CustomFileError ByteString
+  | CustomFileError Text
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
+
 
 data DirStatus
   = DirSuccess
   | DirError DirError
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
+
 
 data DirError
   = DirExists
   | DirDoesNotExist
   | IncorrectDirPath
-  | CustomDirError ByteString
+  | CustomDirError Text
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
+
 
 newtype SystemStatus = SystemStatus Size
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 newtype Size = Size Integer
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
-newtype IP = IP ByteString
+
+newtype IP = IP String
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
+
+
 newtype IPList = IPList [IP]
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
+
 
 data FileInfo = FileInfo Size [IP]
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
+
 
 newtype DirPath = DirPath String
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
+
+
 newtype FileName = FileName String
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
+
 
 newtype DirInfo = DirInfo [FileName]
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
--- shared/MonadFS/FileTree.hs
+++ shared/MonadFS/FileTree.hs
@@ -1,15 +1,22 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
+
 module MonadFS.FileTree where
 
+import           Data.Aeson
 import           Data.ByteString
 import           Data.Map.Strict   (Map)
 import qualified Data.Map.Strict   as Map
+import           GHC.Generics
 import           MonadFS.API.Types
 
 data FileTree a
   = Dir (Map String (FileTree a))
   | Files (Map String a)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 data FileNode = FileNode String [IP]
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 type VFS = FileTree FileNode
 
