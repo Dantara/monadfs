257c5a5c32aba3dd24eeee1411de4f3476b4db94
Mike <super.mvk@yandex.ru>
2020-10-08 20:17:36 +0300

    Add dummy StorageServer implementation

--- storage-server/Main.hs
+++ storage-server/Main.hs
@@ -1,4 +1,69 @@
 module Main where
 
+import qualified Data.Map.Strict as Map (empty)
+import MonadFS.API.StorageServer (StorageServerAPI)
+import MonadFS.API.Types
+import MonadFS.FileTree (FileTree (FileTree))
+import Network.Wai.Handler.Warp (run)
+import Servant
+import Servant.Multipart (MultipartData, Tmp)
+
+storageServerPort :: Int
+storageServerPort = 4000
+
+storageServerApi :: Proxy StorageServerAPI
+storageServerApi = Proxy
+
+storageServer :: Server StorageServerAPI
+storageServer =
+  initController
+    :<|> treeController
+    :<|> statusController
+    :<|> ( fileCreateController
+             :<|> fileReadController
+             :<|> fileWriteController
+             :<|> fileDeleteController
+             :<|> fileCopyController
+             :<|> fileMoveController
+         )
+    :<|> ( dirCreateController :<|> dirDeleteController
+         )
+
 main :: IO ()
-main = putStrLn "Hello world from Storage Server!"
+main = do
+  putStrLn $ "[+] Starting Storage Server on " <> show storageServerPort <> " port."
+  run storageServerPort (serve storageServerApi storageServer)
+
+-- | Controllers
+initController :: Handler StorageServerStatus
+initController = return (StorageServerOk (Size 666))
+
+treeController :: Handler (FileTree FileName)
+treeController = return (FileTree Map.empty Map.empty)
+
+statusController :: Handler StorageServerStatus
+statusController = return (StorageServerOk (Size 666))
+
+fileCreateController :: String -> Handler (FileStatus ())
+fileCreateController _ = return (FileOk ())
+
+fileReadController :: Tagged Handler Application
+fileReadController = serveDirectoryWebApp "/var/www/html"
+
+fileWriteController :: MultipartData Tmp -> Handler (FileStatus ())
+fileWriteController _ = return (FileOk ())
+
+fileDeleteController :: String -> Handler (FileStatus ())
+fileDeleteController _ = return (FileOk ())
+
+fileCopyController :: String -> Handler (FileStatus ())
+fileCopyController _ = return (FileOk ())
+
+fileMoveController :: String -> Handler (FileStatus ())
+fileMoveController _ = return (FileOk ())
+
+dirCreateController :: DirPath -> Handler (DirStatus ())
+dirCreateController _ = return (DirOk ())
+
+dirDeleteController :: DirPath -> Handler (DirStatus ())
+dirDeleteController _ = return (DirOk ())
