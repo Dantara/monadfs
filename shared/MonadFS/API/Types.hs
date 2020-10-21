09f0c4c84e2a16a96cff359e5dfb06d6c313b321
Dantara <supercreed2@gmail.com>
2020-10-21 03:32:38 +0300

    MimeRender & MimeUnrender instances

--- shared/MonadFS/API/Types.hs
+++ shared/MonadFS/API/Types.hs
@@ -1,12 +1,19 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module MonadFS.API.Types where
 
 import           Data.Aeson
-import           Data.ByteString.Lazy
-import           Data.Text            (Text)
-import qualified Data.Text            as T
+import           Data.Bifunctor
+import           Data.ByteString.Lazy    (ByteString)
+import qualified Data.ByteString.Lazy    as BS
+import           Data.Int                (Int64)
+import           Data.String             (fromString)
+import           Data.Text               (Text)
+import qualified Data.Text               as T
+import qualified Data.Text.Lazy.Encoding as T (decodeUtf8')
 import           GHC.Generics
 import           Path
 import           Servant
@@ -67,18 +74,31 @@ data FileInfo = FileInfo Size [ServerAddr]
   deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 
--- newtype DirPath = DirPath String
---   deriving (Eq, Show, Generic, FromJSON, ToJSON)
+newtype AbsDirPath = AbsDirPath (Path Abs Dir)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
-type AbsDirPath = Path Abs Dir
 
-type AbsFilePath = Path Abs File
+newtype AbsFilePath = AbsFilePath (Path Abs File)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
--- instance FromHttpApiData DirPath where
---   parseUrlPiece t = Right $ DirPath $ T.unpack t
 
--- instance ToHttpApiData DirPath where
---   toUrlPiece (DirPath s) = T.pack s
+instance ToHttpApiData AbsDirPath where
+  toUrlPiece (AbsDirPath p) = T.pack $ fromAbsDir p
+
+instance FromHttpApiData AbsDirPath where
+  parseUrlPiece t = either (Left . T.pack . show) (Right . AbsDirPath)
+    $ parseAbsDir
+    $ T.unpack t
+
+
+instance ToHttpApiData AbsFilePath where
+  toUrlPiece (AbsFilePath p) = T.pack $ fromAbsFile p
+
+instance FromHttpApiData AbsFilePath where
+  parseUrlPiece t = either (Left . T.pack . show) (Right . AbsFilePath)
+    $ parseAbsFile
+    $ T.unpack t
+
 
 newtype FileName = FileName String
   deriving (Eq, Ord, Show, Generic, FromJSON,
@@ -104,3 +124,32 @@ data SourceDest = SourceDest AbsFilePath AbsFilePath
 newtype FileContent = FileContent ByteString
 
 data FileWrite = FileWrite AbsFilePath FileContent
+
+
+instance MimeRender OctetStream FileContent where
+  mimeRender _ (FileContent c) = c
+
+instance MimeUnrender OctetStream FileContent where
+  mimeUnrender _ s = Right $ FileContent s
+
+
+maxPathLength :: Int64
+maxPathLength = 4096
+
+instance MimeRender OctetStream FileWrite where
+  mimeRender ctype (FileWrite p c) = encodedPath <> separator <> encodedContent
+    where
+      encodedPath :: ByteString
+      encodedPath = fromString $ show p
+      separatorLength = maxPathLength - BS.length encodedPath
+      separator = BS.replicate separatorLength 0
+      encodedContent = mimeRender ctype c
+
+instance MimeUnrender OctetStream FileWrite where
+  mimeUnrender _ s = either Left (\p -> Right $ FileWrite (AbsFilePath p) content)
+    $ eitherPath >>= pathParser
+    where
+      (encodedPathWithSep, encodedContent) = BS.splitAt maxPathLength s
+      content = FileContent encodedContent
+      eitherPath = bimap show show $ T.decodeUtf8' $ BS.takeWhile (/= 0) encodedPathWithSep
+      pathParser = first show . parseAbsFile
